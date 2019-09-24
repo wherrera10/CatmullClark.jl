21a0df280c75cdbea1d9b24f63b32d1149c4bf29
Bill <wherrera10@users.noreply.github.com>
2019-09-24 02:54:06 -1000

    Create demo.jl

---
+++ example/demo.jl
@@ -0,0 +1,38 @@
+using CatmullClark, CatmullClarkGraphics
+
+const inputpoints = [
+    [-1.0, -1.0, -1.0],
+    [-1.0, -1.0, 1.0],
+    [-1.0, 1.0, -1.0],
+    [-1.0, 1.0, 1.0],
+    [1.0, -1.0, -1.0],
+    [1.0, -1.0, 1.0],
+    [1.0, 1.0, -1.0],
+    [1.0, 1.0, 1.0]
+]
+
+const inputfaces = [
+    [0, 4, 5, 1],
+    [4, 6, 7, 5],
+    [6, 2, 3, 7],
+    [2, 0, 1, 3],
+    [1, 5, 7, 3],
+    [0, 2, 6, 4]
+]
+
+const faces = [map(x -> Point3f0(inputpoints[x]), p .+ 1) for p in inputfaces]
+
+scene = drawfaces(faces, :black)
+display(scene)
+sleep(1)
+
+function displaycallback(faces)
+    drawfaces!(nextfaces, colors[i])
+    display(scene)
+    sleep(1)
+end
+
+catmullclark(faces, 4, displaycallback)
+
+println("Press Enter to continue", readline())
+
