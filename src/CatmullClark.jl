14b81a3c05072e35acfae187005582f5f0a8f067
Bill <wherrera10@users.noreply.github.com>
2019-09-25 07:33:27 -1000

    combine module files

--- src/CatmullClark.jl
+++ src/CatmullClark.jl
@@ -112,3 +112,40 @@ function catmullclark(faces, iters, callback=(x)->0)
 end
 
 end # module
+                                 
+module CatmullClarkGraphics
+
+export drawfaces, drawfaces!, displaycallback, getscene, setscene
+
+using Makie
+
+facewrapped(face) = [face; face[1]]
+drawface(face, colr) = lines(facewrapped(face); color=colr)
+drawface!(face, colr) = lines!(facewrapped(face); color=colr)
+drawfaces!(faces, colr) = for f in faces drawface!(f, colr) end
+const colors = [:red, :green, :blue, :gold]
+const iterconfig = [0, length(colors), nothing]
+
+setscene(s) = (iterconfig[3] = s)
+getscene() = iterconfig[3]
+
+function drawfaces(faces, colr)
+    scene = drawface(faces[1], colr)
+    if length(faces) > 1
+        for f in faces[2:end]
+            drawface!(f, colr)
+        end
+    end
+    setscene(scene)
+    return scene
+end
+
+function displaycallback(faces)
+    drawfaces!(nextfaces, colors[iterconfig[1] % iterconfig[2] + 1])
+    iterconfig[1] +=1
+    iterconfig[3] != nothing && display(iterconfig[3])
+    sleep(1)
+end
+
+end # module
+
