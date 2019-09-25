c34a39c6555af4783b1d9364af20475d2da17266
Bill <wherrera10@users.noreply.github.com>
2019-09-25 12:13:08 -1000

    Create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using Documenter, OhMyREPL
+
+makedocs(
+    sitename = "CatmullClark",
+    pages = Any[
+        "Home" => "index.md",
+        "Installation" => "installation.md",
+        "Features" => Any[
+            "features/syntax_highlighting.md",
+            "features/bracket_highlighting.md",
+            "features/bracket_complete.md",
+            "features/prompt.md",
+            "features/rainbow_brackets.md",
+            "features/markdown_highlight.md",
+            ],
+        "Internals" => Any[
+            "internals/passes.md"
+            ]
+    ]
+)
+
+deploydocs(
+    repo = "github.com/wherrera10/CatmullClark.jl.git",
+)
+
