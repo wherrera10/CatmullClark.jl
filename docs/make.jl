6dcfa44bd262acb442bc4f0a8ca6a16ca5d62212
Bill <wherrera10@users.noreply.github.com>
2019-09-25 12:18:38 -1000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,17 +5,7 @@ makedocs(
     pages = Any[
         "Home" => "index.md",
         "Installation" => "installation.md",
-        "Features" => Any[
-            "features/syntax_highlighting.md",
-            "features/bracket_highlighting.md",
-            "features/bracket_complete.md",
-            "features/prompt.md",
-            "features/rainbow_brackets.md",
-            "features/markdown_highlight.md",
-            ],
-        "Internals" => Any[
-            "internals/passes.md"
-            ]
+        "Functions" => "Functions.md",
     ]
 )
 
