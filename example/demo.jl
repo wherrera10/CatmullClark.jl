255151b935ba4130084fdafeefdc1753e2f0c4fa
Bill <wherrera10@users.noreply.github.com>
2019-09-26 00:25:17 -1000

    Update demo.jl

--- example/demo.jl
+++ example/demo.jl
@@ -8,7 +8,8 @@ const inputpoints = [
     [1.0, -1.0, -1.0],
     [1.0, -1.0, 1.0],
     [1.0, 1.0, -1.0],
-    [1.0, 1.0, 1.0]]
+    [1.0, 1.0, 1.0]
+]
 
 const inputfaces = [
     [0, 4, 5, 1],
@@ -16,7 +17,8 @@ const inputfaces = [
     [6, 2, 3, 7],
     [2, 0, 1, 3],
     [1, 5, 7, 3],
-    [0, 2, 6, 4]]
+    [0, 2, 6, 4]
+]
 
 const donutpoints = [
     [-2.0, -0.5, -2.0], [-2.0, -0.5, 2.0], [2.0, -0.5, -2.0], [2.0, -0.5, 2.0],
@@ -31,23 +33,29 @@ donutfaceindices = [
     [12, 13, 5, 4], [13, 15, 7, 5], [14, 12, 4, 6], [15, 14, 6, 7]]
 
 const faces = [map(x -> Point3f0(inputpoints[x]), p .+ 1) for p in inputfaces]
+const donutfaces = [map(x -> Point3f0(donutpoints[x]), p .+ 1) for p in donutfaceindices]
 
+# cube, rounds toward a shpere
 scene = drawfaces(faces, :black)
 display(scene)
 setscene(scene)
 sleep(1)
-
 catmullclark(faces, 3, CatmullClark.displaycallback)
 
-
-const donutfaces = [map(x -> Point3f0(donutpoints[x]), p .+ 1) for p in donutfaceindices]
-
+# torus
 scene2 = drawfaces(donutfaces, :black)
 display(scene2)
 setscene(scene2)
 sleep(1)
-
 catmullclark(donutfaces, 3, CatmullClark.displaycallback)
+
+# if a face missing in cube, makes cuplike shape
+sleep(3)
+scene3 = drawfaces(faces[2:end], :black)
+display(scene3)
+setscene(scene3)
+sleep(2)
+catmullclark(faces[2:end], 3, CatmullClark.displaycallback)
 
 
 println("Press Enter to continue", readline())
