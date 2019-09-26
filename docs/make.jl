e8716ba2ad5bf6a3b13c2e561b7a35d93f65fca3
Bill <wherrera10@users.noreply.github.com>
2019-09-26 00:56:40 -1000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,6 @@ makedocs(
     sitename="CatmullClark.jl",
     modules=[CatmullClark],
     pages=["index.md", "examples.md", "reference.md", "devnotes.md"],
-    assets=["assets/custom.css"],
 )
 
 deploydocs(repo="github.com/wherrera10/CatmullClark.jl.git")
