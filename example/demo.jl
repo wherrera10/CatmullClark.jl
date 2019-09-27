2d02f2d48d67f8f4a699de575dbfcc940911ee9a
Bill <wherrera10@users.noreply.github.com>
2019-09-27 07:26:41 -1000

    typos in docs

--- example/demo.jl
+++ example/demo.jl
@@ -8,8 +8,7 @@ const inputpoints = [
     [1.0, -1.0, -1.0],
     [1.0, -1.0, 1.0],
     [1.0, 1.0, -1.0],
-    [1.0, 1.0, 1.0]
-]
+    [1.0, 1.0, 1.0]]
 
 const inputfaces = [
     [0, 4, 5, 1],
@@ -17,8 +16,7 @@ const inputfaces = [
     [6, 2, 3, 7],
     [2, 0, 1, 3],
     [1, 5, 7, 3],
-    [0, 2, 6, 4]
-]
+    [0, 2, 6, 4]]
 
 const donutpoints = [
     [-2.0, -0.5, -2.0], [-2.0, -0.5, 2.0], [2.0, -0.5, -2.0], [2.0, -0.5, 2.0],
@@ -35,7 +33,7 @@ donutfaceindices = [
 const faces = [map(x -> Point3f0(inputpoints[x]), p .+ 1) for p in inputfaces]
 const donutfaces = [map(x -> Point3f0(donutpoints[x]), p .+ 1) for p in donutfaceindices]
 
-# cube, rounds toward a shpere
+# cube, rounds toward a sphere
 scene = drawfaces(faces, :black)
 display(scene)
 setscene(scene)
@@ -56,6 +54,5 @@ display(scene3)
 setscene(scene3)
 sleep(2)
 catmullclark(faces[2:end], 3, CatmullClark.displaycallback)
-
 
 println("Press Enter to continue", readline())
